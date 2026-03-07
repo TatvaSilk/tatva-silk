3445c5c95a849f15068397904d787d43feacde20
TatvaSilk <nimeshpatel001@yahoo.com>
2026-03-07 14:19:53 +0300

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,14 @@
 import LoginPanel from '@/components/LoginPanel'
+import AccountProfile from '@/components/AccountProfile'
+
+export const revalidate = 0
 
 export default function AccountPage() {
   return (
-    <main style={{ padding: '40px', maxWidth: 500, margin: '0 auto' }}>
+    <main style={{ padding: '40px', maxWidth: 1080, margin: '0 auto' }}>
       <h1>Account</h1>
       <LoginPanel />
+      <AccountProfile />
     </main>
   )
 }
